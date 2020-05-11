c8fd5500a7d51dff954cf1a439785a015bb2b809
angelvilches <65095097+angelvilches@users.noreply.github.com>
2020-05-11 12:03:25 +0200

    Add files via upload

--- emailconfirmation-rcp.php
+++ emailconfirmation-rcp.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/angelvilches/emailconfirmation-rcp
 Description: This plugin allow you to add a confirmation email field for Restrict Content Pro register form and use the email as username.
 Version: 1.0
 Author: Ángel Vilches
-Author URI: https://angelvilches.com
+Author URI: https://www.angelvilches.com
 License: GPLv2 or later
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:  emailconfirmation-rcp
@@ -18,10 +18,11 @@ if (!defined('ABSPATH'))
 if (!defined('EMAILCONFIRMATION_RCP_TEXTDOMAIN'))
 	define('EMAILCONFIRMATION_RCP_TEXTDOMAIN', 'av_ecrcp');
 
+
 add_action( 'admin_init', 'av_ecrcp_child_plugin_has_parent_plugin' );
 	function av_ecrcp_child_plugin_has_parent_plugin() {
 		if ( is_admin() && current_user_can( 'activate_plugins' ) &&  !is_plugin_active( 'restrict-content-pro/restrict-content-pro.php' ) ) {
-			add_action( 'admin_notices', 'av_ecrcp_child_plugin_notice' );
+			add_action( 'admin_notices', 'av_ecrcp_author_admin_notice' );
 	
 			deactivate_plugins( plugin_basename( __FILE__ ) ); 
 	
@@ -31,13 +32,14 @@ add_action( 'admin_init', 'av_ecrcp_child_plugin_has_parent_plugin' );
 		}
 	}
 	
-	function av_ecrcp_child_plugin_notice(){
-		?><div class="error"><p><?php __('Sorry, but Email confirmation for Restrict Content Pro requires Restrict Content Pro plugin to be installed and active.','emailconfirmation-rcp');?></p></div>
-		<?php
+function av_ecrcp_author_admin_notice(){
+	echo '<div class="notice notice-error is-dismissible">
+			  <p>'.__('Sorry, but Email confirmation for Restrict Content Pro requires Restrict Content Pro plugin to be installed and active.','emailconfirmation-rcp').'</p>
+			 </div>';
 	}
 
 //Archivo de idioma
-//Language file
+//Language
 add_action('plugins_loaded', 'av_ecrcp_load_textdomain');
 
 	function av_ecrcp_load_textdomain() {
@@ -56,26 +58,23 @@ function av_ecrcp_cargacss()
 	wp_enqueue_style('av_cargacss', plugin_dir_url(__FILE__) . 'css/emailconfirmation-rcp.css');
 }
 
-
 //Añadimos un campo de email
-
+// Add email field
 function av_ecrcp_add_email() {
 
     $alternate_email = get_user_meta( get_current_user_id(), 'rcp_alt_email', true );
     ?>
-    <p>
-        <label for="rcp_alt_email"><?php _e( 'Email', 'emailconfirmation-rcp' ); ?></label>
-        <input type="email" id="rcp_alt_email" name="rcp_alt_email" value="<?php echo esc_attr( $alternate_email ); ?>"/>
-    </p>
+<p id="rcp_user_email_wrap">
+<label for="rcp_user_email2"><?php echo apply_filters ( 'rcp_registration_email_label', __( 'Email', 'rcp' ) ); ?></label>
+<input name="rcp_user_email2" id="rcp_user_email2" class="required" type="text" <?php if( isset( $_POST['rcp_user_email2'] ) ) { echo 'value="' . esc_attr( $_POST['rcp_user_email2'] ) . '"'; } ?>/>
+</p>
 
     <?php
 }
 add_action( 'rcp_before_register_form_fields', 'av_ecrcp_add_email' );
 
-
-/**
- * Valida si los emails de ambos campos son idénticos
- */
+// Valida si ambos campos de email son idénticos
+// Check if both email fields match
 function av_ecrcp_validate_email( $posted ) {
 
     if ( is_user_logged_in() ) {
@@ -83,9 +82,18 @@ function av_ecrcp_validate_email( $posted ) {
     }
 
     // Si no coincide con el email del campo rcp_user_email, da error
-    if (  $posted['rcp_alt_email']  !== $posted['rcp_user_email'] ) {
+    if (  $posted['rcp_user_email2']  !== $posted['rcp_user_email'] ) {
 		rcp_errors()->add( 'invalid_alt_email', __( 'Your emails do not match', 'emailconfirmation-rcp' ), 'register' );
     }
 
 }
-add_action( 'rcp_form_errors', 'av_ecrcp_validate_email', 10 );
+add_action( 'rcp_form_errors', 'av_ecrcp_validate_email',99);
+
+// Quitamos la obligatoriedad de campo nombre de usuario en el formulario y usamos el email como nombre de usuario
+// Remove  username requirement on the registration form and use the email address as the username.
+function av_ecrcp_quitarusuario( $user ) {
+	rcp_errors()->remove( 'username_empty' );
+	$user['login'] = $user['email'];
+	return $user;
+}
+add_filter( 'rcp_user_registration_data', 'av_ecrcp_quitarusuario' );
